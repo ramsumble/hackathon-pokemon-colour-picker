c8d5066de947255224426bcf2490ac5382c303ab
ramsumble <ram.sumble@gmail.com>
2023-12-11 22:58:34 +1100

    added hover to colours

--- src/Components/ColourPalet.jsx
+++ src/Components/ColourPalet.jsx
@@ -7,15 +7,35 @@ export function ColourPalet({ colours }) {
     console.log("CLICKED!") //placeholder for API
   }
 
+  const mouseOver = (event) => {
+    const target = event.target;
+    target.style.transform = "scale(1.1)"
+  }
+
+  const mouseLeave = (event) => {
+    const target = event.target;
+    target.style.transform = "scale(1)"
+  }
+
+  
   return (
     <div className="App">
       <header className="App-header">
         <h1>Pokemon Colour Wheel</h1>
 
         <div className="colour-palette" >
+
           {colours.map((colour, index) => (
-            <div key={index} className="colour-dot" style={{ backgroundColor: colour }} onClick={clickColour}></div>
+            <div 
+            key={index} 
+            className="colour-dot" 
+            style={{ backgroundColor: colour }} 
+            onClick={clickColour}
+            onMouseEnter={mouseOver}
+            onMouseLeave={mouseLeave}>
+            </div>
           ))}
+
         </div>
       </header>
     </div>
