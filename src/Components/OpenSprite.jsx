c114cc13ab55a73b8c6a153c2057f40664519847
ramsumble <ram.sumble@gmail.com>
2023-12-15 23:00:50 +1100

    added open/close of sprite onclick, disable linting for line 32

--- src/Components/OpenSprite.jsx
+++ src/Components/OpenSprite.jsx
@@ -9,29 +9,34 @@ const OpenSprite = ({ pokemon, onItemClick }) => {
       const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
       const data = await response.json();
       setPokemonData(data);
+
     } catch (error) {
       console.error('Error fetching Pokemon data:', error);
     }
   };
 
   const handleClick = () => {
-    console.log(`Selected Pokemon: ${pokemon}`);
+    // console.log(`Selected Pokemon: ${pokemon}`);
     if (onItemClick) {
       onItemClick(pokemon);
     }
     setHasBeenClicked(!hasBeenClicked);
   };
 
+    // useEffect to fetch data when hasBeenClicked or pokemon changes
   useEffect(() => {
     if (hasBeenClicked) {
       fetchData();
     }
+    // cheating? ¯\_(ツ)_/¯
+    // eslint-disable-next-line react-hooks/exhaustive-deps 
   }, [hasBeenClicked, pokemon]);
 
   return (
     <div onClick={handleClick}>
       {pokemon}
-      {pokemonData && (
+      {/* Display the Pokemon sprite if hasBeenClicked is true */}
+      {hasBeenClicked && pokemonData && ( 
         <img
           alt="pokemon sprite"
           src={pokemonData.sprites.front_default}
